13b618d853e13cade81114f29423a130ef392f6e
Daniel <cdbautista@outlook.com>
2024-04-09 01:00:42 -0400

    updated main.py

--- main.py
+++ main.py
@@ -2,19 +2,21 @@
 import tcod
 
 from actions import EscapeAction, MovementAction
+from entity import Entity
 from input_handlers import EventHandler
 def main() -> None:
     screen_width = 80
     screen_height = 50
-
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2), int(screen_height /2), "@", (255, 255, 0))
+    entities = {npc, player}
 
     with tcod.context.new_terminal(
         screen_width,
